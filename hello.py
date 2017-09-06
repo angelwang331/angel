fa9b03a9113af037cee3dc819094023acce83055
angelwang331 <55921941@qq.com>
2017-09-06 11:21:05 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print "hello"
+#coding:utf-8
+def add(a=3,b=4):
+    # a=1
+    # b=2
+    c = a+b
+    # return "打印出c的值：%s" %c
+    return c
+
+print add(7,8)
+
+d = add()
+print d
+if d == 3:
+    print "pass"
+else:
+    print "fail"
